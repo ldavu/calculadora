53f2a019fc1fa45a657fe25a6b1c30395ef7de48
ldavu <41762352+ldavu@users.noreply.github.com>
2021-06-02 20:16:44 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.WriteLine("calculadora feita em C#");
+            Console.WriteLine("-----------------------");
+
+            Console.WriteLine("Escreva um numero");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escreva outro número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Escolha o tipo dessa conta");
+            Console.WriteLine("\ta - adição ");
+            Console.WriteLine("\ts - subtração ");
+            Console.WriteLine("\tm - multiplicação ");
+            Console.WriteLine("\td - divisão ");
+
+            operacoes(num1, num2);
+
+            static void operacoes(int num1, int num2)
+            {
+                switch (Console.ReadLine())
+                {
+                    case "a":
+                        Console.WriteLine($"O seu resultado de {num1} + {num2} = " + (num1 + num2));
+                        break;
+
+                    case "s":
+                        Console.WriteLine($"O resultado de {num1} - {num2} = " + (num1 - num2));
+                        break;
+
+                    case "m":
+                        Console.WriteLine($"O resultado de {num1} x {num2} = " + (num1 * num2));
+                        break;
+
+                    case "d":
+                        Console.WriteLine("O resultado de {num1} / {num2} = " + (num1 % num2));
+                        break;
+
+                }   
+            }
+        }
+    }
+}
