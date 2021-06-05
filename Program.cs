ff6345e326bd5a4d4c50f8edb78748d47983d446
ldavu <41762352+ldavu@users.noreply.github.com>
2021-06-05 09:25:31 -0300

    Mudanças
    Coloquei uma repetição para que o usuário possa repetir o calculo novamente

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace calculadora
 {
@@ -6,47 +6,53 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            int num1 = 0;
-            int num2 = 0;
+            var continuar = "s";
+            do
+            {
+                Console.Clear();
+                int num1 = 0;
+                int num2 = 0;
 
-            Console.WriteLine("calculadora feita em C#");
-            Console.WriteLine("-----------------------");
+                Console.WriteLine("Calculadora em C#");
+                Console.WriteLine("-----------------");
 
-            Console.WriteLine("Escreva um numero");
-            num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escreva um numero");
+                num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Escreva outro número");
-            num2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Escreva outro numero");
+                num2 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Escolha a operação");
-            Console.WriteLine("\ta - adição ");
-            Console.WriteLine("\ts - subtração ");
-            Console.WriteLine("\tm - multiplicação ");
-            Console.WriteLine("\td - divisão ");
+                Console.WriteLine("Escolha a operação que deseja fazer");
+                Console.WriteLine("\ta - Adição");
+                Console.WriteLine("\ts - Subtração");
+                Console.WriteLine("\tm - Multiplicação");
+                Console.WriteLine("\td - divisão");
 
-            operacoes(num1, num2);
+                
+                operacoes(num1, num2);
 
+                
+                Console.WriteLine("Deseja continuar? s//n");
+                continuar = Console.ReadLine();
+
+            } while (continuar == "s");
             static void operacoes(int num1, int num2)
             {
                 switch (Console.ReadLine())
                 {
                     case "a":
-                        Console.WriteLine($"O seu resultado de {num1} + {num2} = " + (num1 + num2));
+                        Console.WriteLine($"A soma de {num1} + {num2} = " + (num1 + num2));
                         break;
-
                     case "s":
-                        Console.WriteLine($"O resultado de {num1} - {num2} = " + (num1 - num2));
+                        Console.WriteLine($"A subtração de {num1} - {num2} = " + (num1 - num2));
                         break;
-
                     case "m":
-                        Console.WriteLine($"O resultado de {num1} x {num2} = " + (num1 * num2));
+                        Console.WriteLine($"A multiplicação de {num1} x {num2} = " + (num1 * num2));
                         break;
-
                     case "d":
-                        Console.WriteLine("O resultado de {num1} / {num2} = " + (num1 % num2));
+                        Console.WriteLine($"A divisão de {num1} / {num2} = " + (num1 / num2));
                         break;
-
-                }   
+                }
             }
         }
     }
